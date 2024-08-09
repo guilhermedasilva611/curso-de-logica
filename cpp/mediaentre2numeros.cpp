8a3cb59a511724293dff11124f4a4340f490abc2
Guilherme da Silva Oliveira <118921497+guilhermedasilva611@users.noreply.github.com>
2024-08-09 15:31:40 -0300

    Add files via upload

---
+++ cpp/mediaentre2numeros.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+    using namespace std;
+
+
+int main(){
+
+    float numero1, numero2, media;
+
+    cout << "Digite um valor";
+    cin >> numero1;
+
+    cout << "Digite outro valor";
+    cin >> numero2;
+
+    media = (numero1 + numero2) / 2.0;
+
+    cout << "O valor da media e =" << media;
+
+return 0;
+}
